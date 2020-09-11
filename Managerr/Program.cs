4719b5a5bb088abc936155819fe3793dbd6b7639
Everett Southwick <everett@southwick.dev>
2020-09-11 00:14:26 -0400

    Add waits

--- Managerr/Program.cs
+++ Managerr/Program.cs
@@ -26,18 +26,57 @@ namespace Managerr
             var secondary = RadarrService.BuildRadarrClient(Configuration.GetSection("secondaryRadarr"));
 
             new Timer(async c => await RadarrService.MonitorNewlyReleasedMovies(primary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.MonitorNewlyReleasedMovies(secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.UnmonitorUnreleasedMovies(primary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.UnmonitorUnreleasedMovies(secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.GetOldMissingMovies(primary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.GetOldMissingMovies(secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(30000);
+
             new Timer(async c => await RadarrService.MovieSync(primary, secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(1800000);
+
             new Timer(async c => await RadarrService.MissingMoviesSearch(primary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(1800000);
+
             new Timer(async c => await RadarrService.MissingMoviesSearch(secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(1800000);
+
             new Timer(async c => await RadarrService.CutOffUnmetMoviesSearch(primary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
+            Thread.Sleep(1800000);
+
             new Timer(async c => await RadarrService.CutOffUnmetMoviesSearch(secondary), null, TimeSpan.Zero, TimeSpan.FromDays(1));
 
             Console.ReadLine();
+        }
+
+        public static async Task RepeatActionEvery(Action action,
+          TimeSpan interval, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                action();
+                Task task = Task.Delay(interval, cancellationToken);
+
+                try
+                {
+                    await task;
+                }
+                catch (TaskCanceledException)
+                {
+                    return;
+                }
+            }
         }
     }
 }
